8141024efeeeb39614cb8e8883a3d2eb92af31c5
Kartavikoya <123226436+Kartavikoya@users.noreply.github.com>
2024-11-12 12:28:07 +0530

    Update handsonstubcode.js

--- Session 10/handson1/handsonstubcode.js
+++ Session 10/handson1/handsonstubcode.js
@@ -1,3 +1,12 @@
 // Define a function named power which calculates the result of a base raised to the power of an exponent
 
 // Invoke the function with base 2 and exponent 3 passed as arguments
+function power(base, exponent){
+  var res = 1;
+   for (var i = 1; i <= exponent; i++) {
+          result *= base;
+      }
+    return result;
+}
+var pow = power(2, 3);
+console.log(pow);

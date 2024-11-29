bd35cf6df072a199894656e1b13de020d4b6a20c
Kartavikoya <123226436+Kartavikoya@users.noreply.github.com>
2024-11-29 12:53:10 +0530

    Update handsonstubcode1.js

--- Session 11/handson1/handsonstubcode1.js
+++ Session 11/handson1/handsonstubcode1.js
@@ -3,15 +3,22 @@ const movie = {
     /**
      * TODO: Write setter method to set the title and director of the movie
      */
-    
+    set: function(title, director) {
+        this.title = title;
+        this.director = director;
+    },
 
     /**
      * TODO: Write getter method to print the title and director of the movie
      */
     get: function () {
-        /* TODO: Write an arrow function so that the output from this function is:
+        /* DONE: Wrote an arrow function so that the output from this function is:
         Title = Inception, Director = Christopher Nolan
-        */    
+        */
+        var print = () => {
+            console.log("Title = " + this.title + ", Director = " + this.director);
+        }
+        print();
     }
 }
 
